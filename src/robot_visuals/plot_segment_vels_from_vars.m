da4a7070ca2f47853214bf32d4e41a00d9e36287
Filip Becanovic <becaphilippe@gmail.com>
2024-12-30 17:41:52 +0100

    added ability to plot cartesian velocoties from variables with identical arguments as 'plot'

--- src/robot_visuals/plot_segment_vels_from_vars.m
+++ src/robot_visuals/plot_segment_vels_from_vars.m
@@ -1,4 +1,4 @@
-function plot_segment_vels_from_vars(num_vars)
+function plot_segment_vels_from_vars(num_vars, varargin)
 
 [n, N] = size(num_vars.variables.q);
 t = linspace(0, (N-1) * num_vars.parameters.dt, N);
@@ -6,17 +6,17 @@ t = linspace(0, (N-1) * num_vars.parameters.dt, N);
 for ii = 1 : n
     subplot(3, n, ii)
     hold on;
-    plot(t, num_vars.functions.V{ii+1}(1, :));
+    plot(t, num_vars.functions.V{ii+1}(1, :), varargin{:});
     ylabel(sprintf("$V^{%d}_x$", ii), 'Interpreter', 'latex');
 
     subplot(3, n, n + ii)
     hold on;
-    plot(t, num_vars.functions.V{ii+1}(2, :));
+    plot(t, num_vars.functions.V{ii+1}(2, :), varargin{:});
     ylabel(sprintf("$V^{%d}_y$", ii), 'Interpreter', 'latex');
 
     subplot(3, n, 2*n + ii)
     hold on;
-    plot(t, num_vars.functions.V{ii+1}(3, :));
+    plot(t, num_vars.functions.V{ii+1}(3, :), varargin{:});
     ylabel(sprintf("$V^{%d}_{\\theta}$", ii), 'Interpreter', 'latex');
         
     xlabel(sprintf("$t$ [s]"), 'Interpreter', 'latex')
