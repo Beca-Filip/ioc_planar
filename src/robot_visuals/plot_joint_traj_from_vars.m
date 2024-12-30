e73308e543c33dca9bcdfd873c933469137695da
Filip Becanovic <becaphilippe@gmail.com>
2024-12-30 17:31:06 +0100

    added ability to plot joint trajectories from variables with identical arguments as 'plot'

--- src/robot_visuals/plot_joint_traj_from_vars.m
+++ src/robot_visuals/plot_joint_traj_from_vars.m
@@ -1,4 +1,4 @@
-function plot_joint_traj_from_vars(num_vars)
+function plot_joint_traj_from_vars(num_vars, varargin)
 
 [n, N] = size(num_vars.variables.q);
 t = linspace(0, (N-1) * num_vars.parameters.dt, N);
@@ -6,7 +6,7 @@ t = linspace(0, (N-1) * num_vars.parameters.dt, N);
 for ii = 1 : n
     subplot(n, 4, (ii-1) * 4 + 1)
     hold on;
-    plot(t, num_vars.variables.q(ii, :));
+    plot(t, num_vars.variables.q(ii, :), varargin{:});
     ylabel(sprintf("$q_{%d}$", ii), 'Interpreter', 'latex');
     
     if ii == n
@@ -15,7 +15,7 @@ for ii = 1 : n
 
     subplot(n, 4, (ii-1) * 4 + 2)
     hold on;
-    plot(t(1:end-1), num_vars.variables.dq(ii, :));
+    plot(t(1:end-1), num_vars.variables.dq(ii, :), varargin{:});
     ylabel(sprintf("$\\dot{q}_{%d}$", ii), 'Interpreter', 'latex');
 
     if ii == n
@@ -24,7 +24,7 @@ for ii = 1 : n
     
     subplot(n, 4, (ii-1) * 4 + 3)
     hold on;
-    plot(t(1:end-2), num_vars.variables.ddq(ii, :));
+    plot(t(1:end-2), num_vars.variables.ddq(ii, :), varargin{:});
     ylabel(sprintf("$\\ddot{q}_{%d}$", ii), 'Interpreter', 'latex');
 
     if ii == n
@@ -33,7 +33,7 @@ for ii = 1 : n
     
     subplot(n, 4, (ii-1) * 4 + 4)
     hold on;
-    plot(t(1:end-2), num_vars.functions.model_tau(ii, 1:end-2));
+    plot(t(1:end-2), num_vars.functions.model_tau(ii, 1:end-2), varargin{:});
     ylabel(sprintf("$\\tau_{%d}$", ii), 'Interpreter', 'latex');
 
     if ii == n
