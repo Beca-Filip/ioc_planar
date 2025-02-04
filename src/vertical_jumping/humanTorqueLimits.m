f26dbfc79d2662971c86556917972f2dba63d1ef
Davosku <davidmesaros9905@gmail.com>
2025-02-04 13:15:20 +0100

    doubled human torque limits

--- src/vertical_jumping/humanTorqueLimits.m
+++ src/vertical_jumping/humanTorqueLimits.m
@@ -1,6 +1,6 @@
 function [taulo, tauhi] = humanTorqueLimits(n)
-    taulo = [-126, -168, -185, -67, -46];
-    tauhi = [126,  100,  190,  92,  77];
+    taulo = 2*[-126, -168, -185, -67, -46];
+    tauhi = 2*[126,  100,  190,  92,  77];
 
     taulo = taulo(1:n);
     tauhi = tauhi(1:n);
