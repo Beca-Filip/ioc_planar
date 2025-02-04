6724a0caf511528065381fbc55a017968e1056a5
Filip Becanovic <becaphilippe@gmail.com>
2025-02-04 14:48:47 +0100

    renamed num_vars to vars, numpts to nsnapshots, and added function description

--- src/robot_visuals/plot_snapshots_from_vars.m
+++ src/robot_visuals/plot_snapshots_from_vars.m
@@ -1,28 +1,32 @@
-function plot_snapshots_from_vars(num_vars, numpts)
+function plot_snapshots_from_vars(vars, nsnapshots)
+%PLOT_SNAPSHOTS_FROM_VARS plots snapshots in a single figure.
+%
+%   PLOT_SNAPSHOTS_FROM_VARS(vars, nsnapshots) plots nsnapshots snapshots 
+%   in the same figure. Default value for nsnapshots is 5.
     if nargin < 2
-        numpts = 5;
+        nsnapshots = 5;
     end
     
-    [n, N] = size(num_vars.variables.q);
+    [n, N] = size(vars.variables.q);
     % Plot cartesian EE traj
-    plot(num_vars.functions.P{end}(1, :), num_vars.functions.P{end}(2, :), 'Color', [1, 1, 0], 'LineWidth', 2);
+    plot(vars.functions.P{end}(1, :), vars.functions.P{end}(2, :), 'Color', [1, 1, 0], 'LineWidth', 2);
 
     % Plot cartesian segment COM trajectories
     for ii = 1 : n
-        plot(num_vars.functions.Pcom{ii}(1, :), num_vars.functions.Pcom{ii}(2, :), 'Color', [0, 1, 1], 'LineWidth', 2);
+        plot(vars.functions.Pcom{ii}(1, :), vars.functions.Pcom{ii}(2, :), 'Color', [0, 1, 1], 'LineWidth', 2);
     end
 
     % Plot cartesian total COM trajectory
-    if isfield(num_vars.functions, 'Pcomtotal')
-        plot(num_vars.functions.Pcomtotal(1, :), num_vars.functions.Pcomtotal(2, :), 'Color', [0.2, 0.7, 0.05], 'LineWidth', 2);
+    if isfield(vars.functions, 'Pcomtotal')
+        plot(vars.functions.Pcomtotal(1, :), vars.functions.Pcomtotal(2, :), 'Color', [0.2, 0.7, 0.05], 'LineWidth', 2);
     end
 
     % Plot robot configurations
-    for ii = floor(linspace(1, N, numpts))
-        [P, ~, ~, ~] = snapshot_from_vars(num_vars, ii);
+    for ii = floor(linspace(1, N, nsnapshots))
+        [P, ~, ~, ~] = snapshot_from_vars(vars, ii);
         dP = P(end, :) - P(end-1, :);
         alpha = .2;
         text(P(end, 1) + alpha * dP(1), P(end, 2) + alpha * dP(2), sprintf("%d", ii-1), 'FontSize', 25, 'Interpreter', 'latex', 'FontWeight', 'bold', 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle');
-        snapshot_from_vars(num_vars, ii);
+        snapshot_from_vars(vars, ii);
     end
 end
