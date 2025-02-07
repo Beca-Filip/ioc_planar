9c7bc24655d1101682db85696e2b8958cf1d0789
Davosku <davidmesaros9905@gmail.com>
2025-02-07 15:59:12 +0100

    added numerical com speed to plot function

--- src/robot_visuals/plot_total_com_kinematics_from_vars.m
+++ src/robot_visuals/plot_total_com_kinematics_from_vars.m
@@ -18,16 +18,20 @@ grid;
 
 xlabel("$t$ [s]", 'Interpreter', 'latex')
 
+numeric_Vcomtotal = diff(num_vars.functions.Pcomtotal(:, :),1,2)/num_vars.parameters.dt;
+
 % Velocity
 subplot(2, 2, 2)
 hold on;
 plot(t, num_vars.functions.Vcomtotal(1, :), varargin{:});
+plot(t(1:end-1), numeric_Vcomtotal(1, :), varargin{:});
 ylabel("$V_{{\rm COM Total}, x}$", 'Interpreter', 'latex');
 grid;
 
 subplot(2, 2, 4)
 hold on;
 plot(t, num_vars.functions.Vcomtotal(2, :), varargin{:});
+plot(t(1:end-1), numeric_Vcomtotal(2, :), varargin{:});
 ylabel("$V_{{\rm COM Total}, y}$", 'Interpreter', 'latex');
 grid;
     
