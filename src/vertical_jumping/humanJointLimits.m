ab857b0786f199cfd7694ac227ab1a05ebab5a0c
Filip Becanovic <becaphilippe@gmail.com>
2025-02-11 17:29:55 +0100

    limits functions return different things with a flag indicating foot presence in the model

--- src/vertical_jumping/humanJointLimits.m
+++ src/vertical_jumping/humanJointLimits.m
@@ -1,7 +1,17 @@
-function [qlo, qhi] = humanJointLimits(n)
-   qlo = deg2rad([30,  -5,   -180, -15, 0  ]);
-   qhi = deg2rad([150, 180, 30,   225, 180]);
+function [qlo, qhi] = humanJointLimits(n, footFlag)
+    if nargin < 2
+        footFlag = false;
+    end
+    if ~footFlag
+        qlo = deg2rad([30,  -5,   -180, -225, 0  ]);
+        qhi = deg2rad([150, 180, 30,   15, 180]);
+        qlo = qlo(1:n);
+        qhi = qhi(1:n);
+        return
+    end
 
-   qlo = qlo(1:n);
-   qhi = qhi(1:n);
+    qlo = deg2rad([60, -180,  -5, -180, -225, 0  ]);
+    qhi = deg2rad([180,   0, 180,   30, 15, 180]);
+    qlo = qlo(1:n);
+    qhi = qhi(1:n);
 end
--- src/vertical_jumping/humanTorqueLimits.m
+++ src/vertical_jumping/humanTorqueLimits.m
@@ -1,7 +1,16 @@
-function [taulo, tauhi] = humanTorqueLimits(n)
-    taulo = 2*[-126, -168, -185, -67, -46];
-    tauhi = 2*[126,  100,  190,  92,  77];
-
-    taulo = taulo(1:n);
-    tauhi = tauhi(1:n);
+function [taulo, tauhi] = humanTorqueLimits(n, footFlag)
+    if nargin < 2
+        footFlag = false;
+    end
+    if ~footFlag
+        taulo = 2*[-126, -168, -185, -67, -46];
+        tauhi = 2*[126,  100,  190,  92,  77];
+    
+        taulo = taulo(1:n);
+        tauhi = tauhi(1:n);
+        return
+    end
+    % Assumption: Foot torque ~= ankle torque
+    taulo = 2*[-126, -126, -168, -185, -67, -46];
+    tauhi = 2*[126, 126,  100,  190,  92,  77];
 end
