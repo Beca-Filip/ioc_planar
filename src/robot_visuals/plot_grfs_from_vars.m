96566cf225b6a19082f0b1bd75dfaf60a3b02aaf
Filip Becanovic <becaphilippe@gmail.com>
2025-02-11 12:18:39 +0100

    plots numerically computed velocities, accelerations, and vertical GRFs

--- src/robot_visuals/plot_grfs_from_vars.m
+++ src/robot_visuals/plot_grfs_from_vars.m
@@ -1,7 +1,7 @@
-function plot_grfs_from_vars(num_vars, varargin)
+function plot_grfs_from_vars(vars, varargin)
 
-[~, Ngrf] = size(num_vars.functions.F{1});
-tgrf = linspace(0, (Ngrf-1) * num_vars.parameters.dt, Ngrf);
+[~, Ngrf] = size(vars.functions.F{1});
+tgrf = linspace(0, (Ngrf-1) * vars.parameters.dt, Ngrf);
 
 mulo = .6;
 muhi = 1.;
@@ -9,23 +9,27 @@ mumi = .8;
 
 subplot(3, 1, 1)
 hold on;
-plot(tgrf, num_vars.functions.F{1}(1, :), varargin{:});
+plot(tgrf, vars.functions.F{1}(1, :), varargin{:});
 % Coulomb cone
-plot(tgrf, -mumi .* abs(num_vars.functions.F{1}(2, :)), 'Color', [0, 0.7, 0.7], varargin{:});
-plot(tgrf, mumi .* abs(num_vars.functions.F{1}(2, :)), 'Color', [0.7, 0, 0.7], varargin{:});
+plot(tgrf, -mumi .* abs(vars.functions.F{1}(2, :)), 'Color', [0, 0.7, 0.7], varargin{:});
+plot(tgrf, mumi .* abs(vars.functions.F{1}(2, :)), 'Color', [0.7, 0, 0.7], varargin{:});
 ylabel("$F_{{\rm GRF}, x}$", 'Interpreter', 'latex');
 grid;
 
+vars.functions.verticalGrfFromCom = sum(vars.parameters.M) * (abs(vars.parameters.gravity(2)) + vars.functions.Acomtotal(2, :));
+% NnumA = size(vars.functions.verticalGrfFromCom, 2);
+
 subplot(3, 1, 2)
 hold on;
-plot(tgrf, num_vars.functions.F{1}(2, :), varargin{:});
+plot(tgrf, vars.functions.F{1}(2, :), varargin{:});
+plot(tgrf, vars.functions.verticalGrfFromCom, varargin{:});
 plot(tgrf([1, end]), zeros(1, 2), 'Color', [0, 0.7, 0.7], varargin{:});
 ylabel("$F_{{\rm GRF}, y}$", 'Interpreter', 'latex');
 grid;
 
 subplot(3, 1, 3)
 hold on;
-plot(tgrf, num_vars.functions.F{1}(3, :), varargin{:});
+plot(tgrf, vars.functions.F{1}(3, :), varargin{:});
 ylabel("$\tau_{{\rm GRF}, z}$", 'Interpreter', 'latex');
 grid;
     
--- src/robot_visuals/plot_total_com_kinematics_from_vars.m
+++ src/robot_visuals/plot_total_com_kinematics_from_vars.m
@@ -4,33 +4,60 @@ function plot_total_com_kinematics_from_vars(num_vars, varargin)
 t = linspace(0, (N-1) * num_vars.parameters.dt, N);
 
 % Position
-subplot(2, 2, 1)
+subplot(2, 3, 1)
 hold on;
 plot(t, num_vars.functions.Pcomtotal(1, :), varargin{:});
 ylabel("$P_{{\rm COM Total}, x}$", 'Interpreter', 'latex');
 grid;
 
-subplot(2, 2, 3)
+subplot(2, 3, 4)
 hold on;
 plot(t, num_vars.functions.Pcomtotal(2, :), varargin{:});
 ylabel("$P_{{\rm COM Total}, y}$", 'Interpreter', 'latex');
 grid;
 
-xlabel("$t$ [s]", 'Interpreter', 'latex')
+xlabel("$t$ [s]", 'Interpreter', 'latex');
 
 % Velocity
-subplot(2, 2, 2)
+num_vars.functions.numVcomtot = diff(num_vars.functions.Pcomtotal, 1, 2) ./ num_vars.parameters.dt;
+NnumV = size(num_vars.functions.numVcomtot, 2);
+subplot(2, 3, 2)
 hold on;
 plot(t, num_vars.functions.Vcomtotal(1, :), varargin{:});
+plot(t(1:NnumV), num_vars.functions.numVcomtot(1, :), varargin{:});
 ylabel("$V_{{\rm COM Total}, x}$", 'Interpreter', 'latex');
 grid;
 
-subplot(2, 2, 4)
+subplot(2, 3, 5)
 hold on;
 plot(t, num_vars.functions.Vcomtotal(2, :), varargin{:});
+plot(t(1:NnumV), num_vars.functions.numVcomtot(2, :), varargin{:});
 ylabel("$V_{{\rm COM Total}, y}$", 'Interpreter', 'latex');
 grid;
     
-xlabel("$t$ [s]", 'Interpreter', 'latex')
+xlabel("$t$ [s]", 'Interpreter', 'latex');
+
+% Acceleration
+num_vars.functions.numAcomtot1 = diff(num_vars.functions.Pcomtotal, 2, 2) ./ num_vars.parameters.dt^2;
+NnumA1 = size(num_vars.functions.numAcomtot1, 2);
+num_vars.functions.numAcomtot2 = diff(num_vars.functions.Vcomtotal, 1, 2) ./ num_vars.parameters.dt;
+NnumA2 = size(num_vars.functions.numAcomtot2, 2);
+subplot(2, 3, 3)
+hold on;
+plot(t, num_vars.functions.Acomtotal(1, :), varargin{:});
+plot(t(1:NnumA1), num_vars.functions.numAcomtot1(1, :), varargin{:});
+plot(t(1:NnumA2), num_vars.functions.numAcomtot2(1, :), varargin{:});
+ylabel("$A_{{\rm COM Total}, x}$", 'Interpreter', 'latex');
+grid;
+
+subplot(2, 3, 6)
+hold on;
+plot(t, num_vars.functions.Acomtotal(2, :), varargin{:});
+plot(t(1:NnumA1), num_vars.functions.numAcomtot1(2, 1:NnumA1), varargin{:});
+plot(t(1:NnumA2), num_vars.functions.numAcomtot2(2, 1:NnumA2), varargin{:});
+ylabel("$A_{{\rm COM Total}, y}$", 'Interpreter', 'latex');
+grid;
+    
+xlabel("$t$ [s]", 'Interpreter', 'latex');
 
 end
